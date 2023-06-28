3e237b8cc7d2ca1bbb11302c9fd0c3bddd47d994
ajstrongdev <adamjsalt@icloud.com>
2023-06-28 19:26:19 +0100

    May have forgot chmod

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo cp pylure /usr/bin/
 sudo cp extras/pylure.png /usr/share/pixmaps
 sudo cp extras/pylure.desktop /usr/share/applications/
 cp extras/pylure.desktop ~/.local/share/applications/
+sudo chmod +x /usr/bin/pylure
